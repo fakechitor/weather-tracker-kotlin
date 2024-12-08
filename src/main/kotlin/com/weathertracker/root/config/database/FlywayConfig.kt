fcf76675ceb6106005fa7720cfb0223711808e07
fakechitor <ladasedanbaklajan06@gmail.com>
2024-12-08 03:10:11 +0300

    feat: add FlywayConfig for migrations

---
+++ src/main/kotlin/com/weathertracker/root/config/database/FlywayConfig.kt
@@ -0,0 +1,20 @@
+package com.weathertracker.root.config.database
+
+import org.flywaydb.core.Flyway
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.sql.DataSource
+
+@Configuration
+class FlywayConfig(
+    private val dataSource: DataSource,
+) {
+    @Bean(initMethod = "migrate")
+    fun flyway(): Flyway =
+        Flyway
+            .configure()
+            .dataSource(dataSource)
+            .locations("classpath:db/migration")
+            .schemas("public")
+            .load()
+}
