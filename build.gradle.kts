cf8e0018e3cf4e3b77b414ba08a25016608e6e88
fakechitor <ladasedanbaklajan06@gmail.com>
2024-12-08 03:09:39 +0300

    build: update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,14 @@
 val springVersion = "6.2.0"
+val flywayVersion = "9.16.0"
 
 plugins {
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
+    kotlin("jvm") version "2.1.0"
+    kotlin("plugin.spring") version "2.1.0"
     war
     id("org.springframework.boot") version "3.4.0"
     id("io.spring.dependency-management") version "1.1.6"
-    id("org.flywaydb.flyway") version "9.16.0"
 }
+
 group = "com.weatherTracker"
 version = "0.0.1-SNAPSHOT"
 
@@ -28,13 +29,15 @@ dependencies {
     implementation("org.springframework:spring-web:$springVersion")
     implementation("org.springframework:spring-webmvc:$springVersion")
     implementation("org.thymeleaf:thymeleaf-spring6:3.1.2.RELEASE")
-    implementation("org.flywaydb:flyway-core:9.16.0")
+    implementation("org.flywaydb:flyway-core:$flywayVersion")
     implementation("org.postgresql:postgresql:42.7.2")
-    implementation("com.zaxxer:HikariCP:5.0.1")
     implementation("org.hibernate:hibernate-core:6.6.3.Final")
-    providedCompile("jakarta.servlet:jakarta.servlet-api:6.1.0")
+    implementation("com.zaxxer:HikariCP:5.0.1")
+    implementation("org.slf4j:slf4j-api:2.0.16")
+    compileOnly("jakarta.servlet:jakarta.servlet-api:6.1.0")
+    testImplementation("ch.qos.logback:logback-classic:1.5.12")
     testImplementation("org.jetbrains.kotlin:kotlin-test:2.1.0")
-    testImplementation("junit:junit:3.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
     testImplementation("com.h2database:h2:2.3.232")
 }
 
@@ -42,14 +45,6 @@ kotlin {
     compilerOptions {
         freeCompilerArgs.addAll("-Xjsr305=strict")
     }
-}
-
-flyway {
-    url = System.getenv("DB_URL") ?: "jdbc:postgresql://localhost:5432/weather_tracker"
-    user = System.getenv("DB_USER") ?: "postgres"
-    password = System.getenv("DB_PASSWORD") ?: "postgres"
-    schemas = arrayOf("public")
-    locations = arrayOf("filesystem:src/main/resources/db/migration")
 }
 
 tasks.withType<Test> {
