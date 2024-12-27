13b9311439ba3fbbceec0e225d85c75c6d2f79f6
fakechitor <ladasedanbaklajan06@gmail.com>
2024-12-27 13:44:19 +0300

    feat: add auth exceptions

---
+++ src/main/kotlin/com/weathertracker/root/exception/UserAlreadyExistsException.kt
@@ -0,0 +1,3 @@
+package com.weathertracker.root.exception
+
+class UserAlreadyExistsException : RuntimeException()
---
+++ src/main/kotlin/com/weathertracker/root/exception/UserNotFoundException.kt
@@ -0,0 +1,3 @@
+package com.weathertracker.root.exception
+
+class UserNotFoundException : RuntimeException()
