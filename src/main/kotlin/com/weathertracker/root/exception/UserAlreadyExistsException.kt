7c1fcecd824265fe61ca5d770c0c84aa21c88760
fakechitor <ladasedanbaklajan06@gmail.com>
2025-01-01 15:19:21 +0300

    feat: add constructors for exceptions

--- src/main/kotlin/com/weathertracker/root/exception/UserAlreadyExistsException.kt
+++ src/main/kotlin/com/weathertracker/root/exception/UserAlreadyExistsException.kt
@@ -1,3 +1,15 @@
 package com.weathertracker.root.exception
 
-class UserAlreadyExistsException : RuntimeException()
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "User already exists")
+class UserAlreadyExistsException : RuntimeException {
+    constructor() : super()
+
+    constructor(message: String) : super(message)
+
+    constructor(message: String?, cause: Throwable?) : super(message, cause)
+
+    constructor(cause: Throwable?) : super(cause)
+}
--- src/main/kotlin/com/weathertracker/root/exception/UserNotFoundException.kt
+++ src/main/kotlin/com/weathertracker/root/exception/UserNotFoundException.kt
@@ -1,3 +1,15 @@
 package com.weathertracker.root.exception
 
-class UserNotFoundException : RuntimeException()
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "User not found")
+class UserNotFoundException : RuntimeException {
+    constructor() : super()
+
+    constructor(message: String) : super(message)
+
+    constructor(message: String?, cause: Throwable?) : super(message, cause)
+
+    constructor(cause: Throwable?) : super(cause)
+}
