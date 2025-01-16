2501227fe84b9ba43ef449b29ece98172aa71922
fakechitor <ladasedanbaklajan06@gmail.com>
2025-01-16 16:20:59 +0300

    refactor: extension functions for validation and encrypting

--- src/main/kotlin/com/weathertracker/root/service/AuthService.kt
+++ src/main/kotlin/com/weathertracker/root/service/AuthService.kt
@@ -21,17 +21,21 @@ class AuthService(
         sessionInfoDto: SessionInfoDto,
         signupUserDto: SignupUserDto,
     ) {
-        // TODO make passwords mask
-        // TODO try to move validation
-        throwIfPasswordsNotEqual(signupUserDto.password, signupUserDto.confirmPassword)
+        signupUserDto.throwIfPasswordsNotEqual()
         sessionService.createSessionAndAddCookie(
             sessionInfoDto = sessionInfoDto,
-            user = userService.saveUser(LoginUserDto(username = signupUserDto.username, password = encryptPassword(signupUserDto))),
+            user =
+                userService.saveUser(
+                    LoginUserDto(username = signupUserDto.username, password = signupUserDto.password?.encryptPassword()),
+                ),
         )
     }
 
-    private fun encryptPassword(signupUserDto: SignupUserDto): String =
-        BCrypt.withDefaults().hashToString(10, signupUserDto.password?.toCharArray())
+    private fun SignupUserDto.throwIfPasswordsNotEqual() {
+        if (this.password != this.confirmPassword) throw PasswordMismatchException("Passwords don't match")
+    }
+
+    private fun String.encryptPassword(): String = BCrypt.withDefaults().hashToString(10, this.toCharArray())
 
     fun logout(
         response: HttpServletResponse,
@@ -46,26 +50,10 @@ class AuthService(
         sessionInfoDto: SessionInfoDto,
     ) {
         userService.findByLogin(loginUserDto)?.let { user ->
-            sessionService.createSessionAndAddCookie(sessionInfoDto, user).takeIf { isPasswordValid(loginUserDto, user) }
+            sessionService.createSessionAndAddCookie(sessionInfoDto, user).takeIf { loginUserDto.password.isPasswordValid(user) }
                 ?: throw IncorrectPasswordException("Password is incorrect")
         } ?: throw UserNotFoundException("User not found")
     }
 
-    private fun isPasswordValid(
-        loginUserDto: LoginUserDto,
-        user: User,
-    ): Boolean =
-        BCrypt
-            .verifyer()
-            .verify(
-                loginUserDto.password?.toCharArray(),
-                user.password,
-            ).verified
-
-    private fun throwIfPasswordsNotEqual(
-        password: String?,
-        confirmPassword: String?,
-    ) {
-        if (password != confirmPassword) throw PasswordMismatchException("Passwords don't match")
-    }
+    private fun String?.isPasswordValid(user: User): Boolean = BCrypt.verifyer().verify(this?.toCharArray(), user.password).verified
 }
