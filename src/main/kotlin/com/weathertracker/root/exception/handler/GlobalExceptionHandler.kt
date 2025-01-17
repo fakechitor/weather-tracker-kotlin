06c8dadedf0957ed3f92842791f8a202c473a633
fakechitor <ladasedanbaklajan06@gmail.com>
2025-01-17 19:55:17 +0300

    feat: add exception for existing locations

--- src/main/kotlin/com/weathertracker/root/exception/handler/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/weathertracker/root/exception/handler/GlobalExceptionHandler.kt
@@ -12,22 +12,21 @@ class GlobalExceptionHandler {
     fun handleUserNotFoundException(e: UserNotFoundException): ModelAndView = ModelAndView("login").apply { addObject("error", e.message) }
 
     @ExceptionHandler(UserAlreadyExistsException::class)
-    fun handleUserAlreadyExistsException(e: UserAlreadyExistsException): ModelAndView =
-        ModelAndView("sign_up").apply { addObject("error", e.message) }
+    fun handleUserAlreadyExistsException(e: UserAlreadyExistsException) = ModelAndView("sign_up").apply { addObject("error", e.message) }
 
     @ExceptionHandler(IncorrectPasswordException::class)
-    fun handleIncorrectPasswordException(e: IncorrectPasswordException): ModelAndView =
-        ModelAndView("login").apply { addObject("error", e.message) }
+    fun handleIncorrectPasswordException(e: IncorrectPasswordException) = ModelAndView("login").apply { addObject("error", e.message) }
 
     @ExceptionHandler(PasswordMismatchException::class)
-    fun handleIllegalArgumentException(e: PasswordMismatchException): ModelAndView =
-        ModelAndView("sign_up").apply { addObject("error", e.message) }
+    fun handleIllegalArgumentException(e: PasswordMismatchException) = ModelAndView("sign_up").apply { addObject("error", e.message) }
 
     @ExceptionHandler(ConstraintViolationException::class)
-    fun handleConstraintViolationException(e: ConstraintViolationException): ModelAndView =
-        ModelAndView("error").apply { addObject("error", e.message) }
+    fun handleConstraintViolationException(e: ConstraintViolationException) = ModelAndView("error").apply { addObject("error", e.message) }
 
     @ExceptionHandler(OpenWeatherApiException::class)
-    fun handleOpenWeatherApiException(e: OpenWeatherApiException): ModelAndView =
+    fun handleOpenWeatherApiException(e: OpenWeatherApiException) = ModelAndView("error").apply { addObject("error", e.message) }
+
+    @ExceptionHandler(LocationAlreadyExistsException::class)
+    fun handleLocationAlreadyExistsException(e: LocationAlreadyExistsException) =
         ModelAndView("error").apply { addObject("error", e.message) }
 }
