4012b11769cf63c547482082888667041e289591
fakechitor <ladasedanbaklajan06@gmail.com>
2025-01-17 21:21:57 +0300

    feat: add search controller

---
+++ src/main/kotlin/com/weathertracker/root/controller/SearchController.kt
@@ -0,0 +1,38 @@
+package com.weathertracker.root.controller
+
+import com.weathertracker.root.dto.LocationDto
+import com.weathertracker.root.service.AuthService
+import com.weathertracker.root.service.LocationService
+import com.weathertracker.root.service.WeatherService
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.*
+
+@Controller
+class SearchController(
+    private val weatherService: WeatherService,
+    private val locationService: LocationService,
+    private val authService: AuthService,
+) {
+    @GetMapping("/search")
+    fun get(
+        @RequestParam city: String,
+        @CookieValue(name = "session_id") sessionId: String,
+        model: Model,
+    ): String {
+        model["user"] = authService.getUserIfAuthorized(sessionId)
+        model["location"] = weatherService.getWeatherInfoForCitiesByCityName(city)
+        println()
+        return "search"
+    }
+
+    @PostMapping("/search")
+    fun saveLocation(
+        @RequestParam userId: Int,
+        @ModelAttribute locationDto: LocationDto,
+    ): String {
+        locationService.saveLocationForUser(locationDto = locationDto, userId = userId)
+        return "redirect:/"
+    }
+}
