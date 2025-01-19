241f0203cb95132cd6a3a8b43ec5fae3a352484e
fakechitor <ladasedanbaklajan06@gmail.com>
2025-01-20 00:55:06 +0300

    feat: additional validation for login/signup

--- src/main/kotlin/com/weathertracker/root/dto/LoginUserDto.kt
+++ src/main/kotlin/com/weathertracker/root/dto/LoginUserDto.kt
@@ -1,10 +1,13 @@
 package com.weathertracker.root.dto
 
+import jakarta.validation.constraints.Pattern
 import jakarta.validation.constraints.Size
 
 data class LoginUserDto(
     @get:Size(min = 3, max = 30, message = "The size should be between 3 and 30")
+    @get:Pattern(regexp = "^[a-zA-Z0-9]+$", message = "The field must contain only numbers and english letters")
     val username: String? = null,
     @get:Size(min = 3, max = 30, message = "The size should be between 3 and 30")
+    @get:Pattern(regexp = "^[a-zA-Z0-9]+$", message = "The field must contain only numbers and english letters")
     val password: String? = null,
 )
--- src/main/kotlin/com/weathertracker/root/dto/SignupUserDto.kt
+++ src/main/kotlin/com/weathertracker/root/dto/SignupUserDto.kt
@@ -5,11 +5,14 @@ import jakarta.validation.constraints.Size
 
 data class SignupUserDto(
     @get:Size(min = 3, max = 30, message = "The size should be between 3 and 30")
+    @get:Pattern(regexp = "^[a-zA-Z0-9]+$", message = "The field must contain only numbers and english letters")
     val username: String? = null,
     @get:Size(min = 3, max = 30, message = "The size should be between 3 and 30")
     @get:Pattern(regexp = ".*[a-zA-Z]+.*", message = "The field must contain at least one letter")
+    @get:Pattern(regexp = "^[a-zA-Z0-9]+$", message = "The field must contain only numbers and english letters")
     val password: String? = null,
     @get:Size(min = 3, max = 30, message = "The size should be between 3 and 30")
     @get:Pattern(regexp = ".*[a-zA-Z]+.*", message = "The field must contain at least one letter")
+    @get:Pattern(regexp = "^[a-zA-Z0-9]+$", message = "The field must contain only numbers and english letters")
     val confirmPassword: String? = null,
 )
