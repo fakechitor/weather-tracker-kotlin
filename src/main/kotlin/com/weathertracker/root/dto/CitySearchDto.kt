d798a306b5e6d193514b9339e911575c8fe7ca79
fakechitor <ladasedanbaklajan06@gmail.com>
2025-01-20 19:31:38 +0300

    fix: change size limits of CitySearchDto

--- src/main/kotlin/com/weathertracker/root/dto/CitySearchDto.kt
+++ src/main/kotlin/com/weathertracker/root/dto/CitySearchDto.kt
@@ -5,6 +5,6 @@ import jakarta.validation.constraints.Size
 
 data class CitySearchDto(
     @get:NotBlank(message = "Field can not be blank")
-    @get:Size(min = 3, max = 30, message = "Field must have between 3 and 30")
+    @get:Size(min = 1, max = 30, message = "Field must have between 1 and 30")
     val city: String = "",
 )
